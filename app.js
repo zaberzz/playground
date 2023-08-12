b66b829053a1fce6b664b0208113ea5aa5bcaaaf
Saba <saba.sadegh67@gmail.com>
2023-08-12 21:53:49 +0430

    add functionality to reset btn

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-Vue.
+Vue.createApp({
+    data(){
+        return
+        {perspective: 100;
+            rotateX: 0;
+            rotateY: 0;
+            rotateZ: 0
+        }
+    }
+}).mount('#app')
